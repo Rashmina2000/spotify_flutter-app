829ca59539d71f6f4302a403af1e21892d53760c
Rashmina Amarasinghe <shasheenrashmina@gmail.com>
2024-10-14 14:19:02 +0530

    splash page

---
+++ lib/presentation/splash/pages/splash.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:spotify/core/configs/assets/app_vectors.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SvgPicture.asset(AppVectors.logo),
+        // Because we cant get .svg vector files with Image.assets();
+      ),
+    );
+  }
+}
