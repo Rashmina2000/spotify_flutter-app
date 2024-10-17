f454bc1ebfbf5af4dab07ffd0da59601d86ee71d
Rashmina Amarasinghe <shasheenrashmina@gmail.com>
2024-10-17 14:14:13 +0530

    get_it library

---
+++ lib/data/repository/auth/auth_repositoryImp.dart
@@ -0,0 +1,13 @@
+import 'package:spotify/domain/repository/auth/auth.dart';
+
+class AuthRepositoryImp implements AuthRepository {
+  @override
+  Future<void> signIn() {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> signUp() {
+    throw UnimplementedError();
+  }
+}
--- lib/data/sources/auth/auth_firebase_service.dart
+++ lib/data/sources/auth/auth_firebase_service.dart
@@ -1,5 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:spotify/data/models/auth/create_user_request.dart';
+
 abstract class AuthFirebaseService {
-  Future<void> signUp();
+  Future<void> signUp(CreateUserRequest createUserReq);
   Future<void> signIn();
 }
 
@@ -10,7 +13,12 @@ class AuthFirebaseServiceImpl implements AuthFirebaseService {
   }
 
   @override
-  Future<void> signUp() {
-    throw UnimplementedError();
+  Future<void> signUp(CreateUserRequest createUserReq) async {
+    try {
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: createUserReq.email,
+        password: createUserReq.password,
+      );
+    } on FirebaseAuthException catch (e) {}
   }
 }
