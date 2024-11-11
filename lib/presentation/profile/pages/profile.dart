00cde42607f212f45bba88b7e00ee3d912c4d5cf
Rashmina Amarasinghe <shasheenrashmina@gmail.com>
2024-11-11 20:46:53 +0530

    display profile image

--- lib/presentation/profile/pages/profile.dart
+++ lib/presentation/profile/pages/profile.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:spotify/common/helpers/is_dark_mode.dart';
 import 'package:spotify/common/widgets/appbar/appbar.dart';
+import 'package:spotify/presentation/profile/bloc/profile_info_cubit.dart';
+import 'package:spotify/presentation/profile/bloc/profile_info_state.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
@@ -21,14 +24,48 @@ class ProfilePage extends StatelessWidget {
   }
 
   Widget _profileInfo(BuildContext context) {
-    return Container(
-      height: MediaQuery.of(context).size.height / 3.5,
-      decoration: BoxDecoration(
-        color: context.isDarkMode ? Color(0xFF2C2828) : Colors.white,
-        borderRadius: BorderRadius.only(
-          bottomRight: Radius.circular(30),
-          bottomLeft: Radius.circular(30),
+    return BlocProvider(
+      create: (context) => ProfileInfoCubit()..getUser(),
+      child: Container(
+        height: MediaQuery.of(context).size.height / 3.5,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: context.isDarkMode ? Color(0xFF2C2828) : Colors.white,
+          borderRadius: BorderRadius.only(
+            bottomRight: Radius.circular(30),
+            bottomLeft: Radius.circular(30),
+          ),
         ),
+        child: BlocBuilder<ProfileInfoCubit, ProfileInfoState>(
+            builder: (context, state) {
+          if (state is ProfileInfoLoading) {
+            return Container(
+              alignment: Alignment.center,
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (state is ProfileInfoLoaded) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 110,
+                  width: 110,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: NetworkImage(state.userEntity.profileURL!),
+                    ),
+                  ),
+                ),
+              ],
+            );
+          }
+          if (state is ProfileInfoFailure) {
+            return const Text("Please try agin");
+          }
+          return Container();
+        }),
       ),
     );
   }
